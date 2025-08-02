533dffb9b1f31c42ef65ef5f3f09ffa86702cc8e
aymaan <aymaanpathan5@gmail.com>
2025-08-02 12:51:03 +0530

    add live audio from socket

--- Backend/controllers/call/message_in_call_controller.py
+++ Backend/controllers/call/message_in_call_controller.py
@@ -1,12 +1,17 @@
+from fastapi import APIRouter, Request, Response
 
-from fastapi import Response
-async def voice():
-    twiml = """
+
+async def message_in_call(req:Request):
+    host = req.headers.get("host") 
+    stream_url = f"wss://{host}/ws"
+
+    twiml = f"""
     <Response>
-        <Say voice="alice">You are now connected. This call will be recorded.</Say>
-        <Pause length="1"/>
-        <Record maxLength="300" timeout="5" transcribe="false" />
-        <Hangup/>
+        <Start>
+            <Stream url="{stream_url}" />  
+        </Start>
+        <Say voice="alice" language="en-US">Hello from call AI</Say>
+        <Pause length="30"/>
     </Response>
     """
-    return Response(content=twiml, media_type="application/xml")
+    return Response(content=twiml.strip(), media_type="text/xml")
